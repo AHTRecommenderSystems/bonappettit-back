34ab9b2a854840896d65d7960eb2d3df79d7b76b
hector9317 <hbautistaj@gmail.com>
2016-04-25 16:55:34 -0500

    Rename category for characteristic

--- src/main/java/com/aht/bonappettit/repository/node/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aht.bonappettit.repository.node;
-
-import com.aht.bonappettit.domain.node.Category;
-import org.springframework.data.neo4j.repository.GraphRepository;
-
-public interface CategoryRepository extends GraphRepository<Category> {
-
-}
