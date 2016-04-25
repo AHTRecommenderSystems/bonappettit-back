a9931fad7ddc41f07c0373c50fe0bb22ff36d0e7
hector9317 <hbautistaj@gmail.com>
2016-04-25 16:56:33 -0500

    Deployin spring context & adding jersey configuration

---
+++ src/main/java/com/aht/bonappettit/configuration/AppInitializer.java
@@ -0,0 +1,39 @@
+package com.aht.bonappettit.configuration;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.aht.bonappettit.Application;
+import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext container) throws ServletException {
+		FilterRegistration.Dynamic encodingFilter = container.addFilter("encoding-filter", new CharacterEncodingFilter());
+	    encodingFilter.setInitParameter("encoding", "UTF-8");
+	    encodingFilter.setInitParameter("forceEncoding", "true");
+	    encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+				
+		final AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(Application.class);
+		context.setServletContext(container);
+		
+	    container.addListener(new ContextLoaderListener(context));  
+	    container.addListener(new RequestContextListener());  
+
+	    final SpringServlet servlet = new SpringServlet();  
+	    final ServletRegistration.Dynamic jerseyServlet = container.addServlet("jersey-serlvet", servlet);  
+	    jerseyServlet.addMapping("/rest/*");  
+	    jerseyServlet.addMapping("/application.wadl");  
+	    jerseyServlet.setInitParameter("com.sun.jersey.config.property.packages", "com.aht.bonappettit.webservices");  
+	    container.setInitParameter("contextConfigLocation", "");  
+	}
+}
