6f03559922fc4d8c15585df2196640d80226b5e2
hector9317 <hbautistaj@gmail.com>
2016-04-27 16:11:00 -0500

    Updating neighbor service

--- src/main/java/com/aht/bonappettit/service/relationship/NeighborService.java
+++ src/main/java/com/aht/bonappettit/service/relationship/NeighborService.java
@@ -5,8 +5,8 @@ import java.util.List;
 import com.aht.bonappettit.domain.relationship.Neighbor;
 
 public interface NeighborService {
-	public void create(Neighbor neighbor);
 	public Neighbor retrieve(long id);
+	public void create(Neighbor neighbor);
 	public void update(Neighbor neighbor);
 	public void delete(Neighbor neighbor);
 	public List<Neighbor> retrieveAll();
