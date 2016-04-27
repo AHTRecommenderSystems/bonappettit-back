46b8fb7cced1a196e055d1bf6d8b980c779e374c
hector9317 <hbautistaj@gmail.com>
2016-04-27 16:10:22 -0500

    Adding restaurant service

--- src/main/java/com/aht/bonappettit/service/node/RestaurantService.java
+++ src/main/java/com/aht/bonappettit/service/node/RestaurantService.java
@@ -5,10 +5,10 @@ import java.util.LinkedList;
 import com.aht.bonappettit.domain.node.Restaurant;
 
 public interface RestaurantService {
+	public void delete(long id);
 	public Restaurant create(Restaurant restaurant);
 	public Restaurant retrieve(long id);
 	public void update(Restaurant restaurant);
 	public void delete(Restaurant restaurant);
-	public void delete(long id);
 	public LinkedList<Restaurant> retrieveAll();
 }
