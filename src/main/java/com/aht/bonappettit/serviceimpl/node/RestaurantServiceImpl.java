f568419f81fe9607cb119485bd04a2fb8a8e0b32
hector9317 <hbautistaj@gmail.com>
2016-04-27 16:08:56 -0500

    Adding service interface and implementation for restaurant

---
+++ src/main/java/com/aht/bonappettit/serviceimpl/node/RestaurantServiceImpl.java
@@ -0,0 +1,40 @@
+package com.aht.bonappettit.serviceimpl.node;
+
+import java.util.LinkedList;
+import org.neo4j.ogm.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aht.bonappettit.repository.node.RestaurantRepository;
+import com.aht.bonappettit.service.node.RestaurantService;
+import com.aht.bonappettit.domain.node.Restaurant;
+
+@Service
+public class RestaurantServiceImpl implements RestaurantService{
+	@Autowired Session session;
+	@Autowired RestaurantRepository repository;
+
+	public Restaurant create(Restaurant restaurant) {
+		return repository.save(restaurant);
+	}
+
+	public Restaurant retrieve(long id) {
+		return repository.findOne(id);
+	}
+
+	public void update(Restaurant restaurant) {
+		repository.save(restaurant, restaurant.getId().intValue());
+	}
+
+	public void delete(Restaurant restaurant) {
+		repository.delete(restaurant);
+	}
+
+	public void delete(long id) {
+		repository.delete(id);
+	}
+
+	public LinkedList<Restaurant> retrieveAll() {
+		return new LinkedList<>(session.loadAll(Restaurant.class));
+	}
+}
