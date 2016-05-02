b396be5b0902854271d2b2bf836c1513d0d4e059
hector9317 <hbautistaj@gmail.com>
2016-05-02 09:40:55 -0500

    Adding upload file to create method

--- src/main/java/com/aht/bonappettit/webservices/node/DishWS.java
+++ src/main/java/com/aht/bonappettit/webservices/node/DishWS.java
@@ -1,6 +1,5 @@
 package com.aht.bonappettit.webservices.node;
 
-import java.util.LinkedList;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,18 +11,88 @@ import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.aht.bonappettit.serviceimpl.node.DishServiceImpl;
 import com.aht.bonappettit.domain.node.Dish;
+import java.util.LinkedList;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
 @Component
 @Path("/dishws")
 public class DishWS {
+	@SuppressWarnings("unused")
+	private static final String directory = "/Users/hector9317/Documents/workspace/bonappettit-back/src/main/resources/";
+	
 	@Autowired DishServiceImpl dishService;
+
+	/*
+	public boolean createFile(InputStream inputStream, String directory) {
+		try {
+			int read = 0;
+			byte[] bytes = new byte[1024];
+			OutputStream out = new FileOutputStream(new File(directory));
+			out = new FileOutputStream(new File(directory));
+			while ((read = inputStream.read(bytes)) != -1)
+				out.write(bytes, 0, read);
+			out.flush();
+			out.close();
+			return true;
+		} catch (IOException e) { return false; }
+	}
+
+	public String hashFunction(String fileName, String dishName) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		String date = dateFormat.format(new Date());
+		String name = fileName + dishName + date; 
+		try {
+			MessageDigest md = MessageDigest.getInstance("md5");
+			md.update(name.getBytes(), 0, name.length());
+			String hashed = new BigInteger(1, md.digest()).toString(64);
+			return hashed;
+		} catch(Exception exception) {
+			return null;
+		}
+	}
 
 	@POST
 	@Path("/create")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response create(@FormParam("name") String name, @FormParam("picture") String picture,
+	public Response create(
+			@FormParam("name") String name,
+			@FormParam("description") String description,
+			@FormDataParam("file") InputStream inputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+			
+		Dish dish = new Dish();
+		JSONObject response = new JSONObject();
+		try {
+			if(name != null)
+				dish.setName(name);
+			if(description != null)
+				dish.setDescription(description);
+			String fileName = hashFunction(name, fileDetail.getFileName());
+			if(fileName != null && createFile(inputStream, directory + fileName)) {
+				System.out.println("Se creó el archivo, ahora el nodo");
+				dish.setPicture(fileName);
+			}
+			dish = dishService.create(dish);
+			if(dish.getId() != null){
+				response.put("id", dish.getId());
+				response.put("success", true);
+			}
+		} catch(Exception exception) {
+			response.put("success", false);
+		}
+		return Response.status(200).entity(response.toString()).build();
+	}
+
+	 */
+
+	
+	@POST
+	@Path("/create2")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response create(
+			@FormParam("name") String name, 
+			@FormParam("picture") String picture,
 			@FormParam("description") String description) {
 		JSONObject response = new JSONObject();
 		Dish dish = new Dish();
